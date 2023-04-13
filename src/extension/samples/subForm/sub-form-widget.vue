026c2f00e930b6403a29ecaa42bdf95a7e766dde
Mercutio <mercutio_john@foxmail.com>
2023-04-13 14:03:34 +0800

    主子表测试

--- src/extension/samples/subForm/sub-form-widget.vue
+++ src/extension/samples/subForm/sub-form-widget.vue
@@ -7,21 +7,82 @@
               :center="field.options.center" :close-text="field.options.closeText"
               :show-icon="field.options.showIcon" :effect="field.options.effect" @close="handleCloseCustomEvent"></el-alert>
 
-    <div class="gantt-chart">
-      <el-table :data="data" style="width: 100%;" :height="tableHeight">
-        <el-table-column prop="name" label="任务名称" fixed="left" width="180"></el-table-column>
-        <el-table-column prop="start" label="开始时间">
-          <template slot-scope="scope">{{ formatDate(scope.row.start) }}</template>
-        </el-table-column>
-        <el-table-column prop="end" label="结束时间" fixed="right" width="120">
-          <template slot-scope="scope">{{ formatDate(scope.row.end) }}</template>
-        </el-table-column>
-        <el-table-column label="甘特图">
-          <template slot-scope="scope">
-            <div class="gantt-item" :style="{width: getWidth(scope.row)}"></div>
-          </template>
-        </el-table-column>
-      </el-table>
+    <div class="sub-form">
+      <el-table
+          :data="columnForm.tableCreatorTableColumnList"
+          :row-class-name="rowTableCreatorTableColumnIndex"
+          @selection-change="handleTableCreatorTableColumnSelectionChange"
+          row-key="uuid"
+          ref="tableCreatorTableColumn"
+          class="column-table"
+          highlight-current-row
+          border
+        >
+          <!--  height="calc(100vh - 220px)" -->
+          <!-- <el-table-column type="selection" width="50" align="center" /> -->
+          <el-table-column label="" align="center" width="40" class-name="allowDrag">
+            <template>
+              <div class="drag-line-box">
+                <i class="drag-line"></i>
+                <i class="drag-line"></i>
+                <i class="drag-line"></i>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="序号" align="center" prop="index" width="60" class-name="allowDrag" />
+          <!-- <el-table-column label="uuid" align="center" prop="uuid" width="60" class-name="allowDrag" /> -->
+          <el-table-column label="字段名" prop="name">
+            <template slot-scope="scope">
+              <CommonFormItem :prop="'tableCreatorTableColumnList.' + scope.$index + '.name'" required style="margin-bottom:0"
+                :rules="[
+                validString(1, 255),
+                {
+                  pattern:/(^_([a-zA-Z0-9]_?)*$)|(^[a-zA-Z](_?[a-zA-Z0-9])*_?$)/,
+                  message:'不符合数据库表字段格式规范'
+                }]"
+              >
+                <el-input v-model="scope.row.name" placeholder="" />
+              </CommonFormItem>
+            </template>
+          </el-table-column>
+          <el-table-column label="字段描述" prop="comment">
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.comment" placeholder="" />
+            </template>
+          </el-table-column>
+          <el-table-column label="数据类型" prop="type"> <!-- width="200"  -->
+            <template slot-scope="scope">
+              <CommonFormItem :prop="'tableCreatorTableColumnList.' + scope.$index + '.type'" required style="margin-bottom:0">
+                <el-select v-model="scope.row.type" placeholder="">
+                  <el-option v-for="dict in options.tableDataTypeOptions" :key="dict.code" :label="dict.name" :value="dict.code" />
+                </el-select>
+              </CommonFormItem>
+            </template>
+          </el-table-column>
+          <el-table-column label="长度" prop="length" width="200" align="center">
+            <template slot-scope="scope">
+              <CommonFormItem :prop="'tableCreatorTableColumnList.' + scope.$index + '.name'" :required="!ifLengthDisabledType(scope.row.type)" style="margin-bottom:0">
+                <el-input-number :controls="false" :min="1" v-model="scope.row.length" :disabled="ifLengthDisabledType(scope.row.type)" /> <!-- :placeholder="ifLengthDisabledType(scope.row.type)?'':'请输入字段长度'"  -->
+              </CommonFormItem>
+            </template>
+          </el-table-column>
+          <el-table-column label="非空" prop="isNull" width="70" align="center">
+            <template slot-scope="scope">
+              <!-- <el-input v-model="scope.row.isNull" placeholder="请输入是否为空" /> -->
+              <el-checkbox true-label="1" v-model="scope.row.isNull"></el-checkbox>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right" width="120">
+            <template slot-scope="scope">
+              <el-tooltip class="item" effect="dark" content="下方添加行" placement="top-end">
+                <el-button ton size="mini" type="text" icon="el-icon-plus" @click="addBetweenRow(scope.row.index)"></el-button>
+              </el-tooltip>
+              <el-tooltip class="item" effect="dark" content="删除当前行" placement="top-end">
+                <el-button size="mini" type="text" icon="el-icon-delete" @click="deleteRow(scope.row.index)"></el-button>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+        </el-table>
     </div>
   </static-content-wrapper>
 </template>
@@ -65,33 +126,14 @@
     components: {
       StaticContentWrapper,
     },
-        data () {
+    data () {
       return {
-        data: [
-          {
-            name: "任务1",
-            start: new Date("2023-04-10T00:00:00"),
-            end: new Date("2023-04-16T00:00:00")
-          },
-          {
-            name: "任务2",
-            start: new Date("2023-04-13T00:00:00"),
-            end: new Date("2023-04-20T00:00:00")
-          },
-          {
-            name: "任务3",
-            start: new Date("2023-04-17T00:00:00"),
-            end: new Date("2023-04-28T00:00:00")
-          },
-          {
-            name: "任务4",
-            start: new Date("2023-04-21T00:00:00"),
-            end: new Date("2023-04-30T00:00:00")
-          }
-        ],
-        startDate: new Date("2023-04-01T00:00:00"),
-        endDate: new Date("2023-05-01T00:00:00"),
-        tableHeight: 400
+        columnForm: {
+          tableCreatorTableColumnList: [],
+        },
+        options: {
+          tableDataTypeOptions: [],
+        },
       }
     },
     created() {
@@ -108,28 +150,100 @@
           changeFn.call(this)
         }
       },
-      getWidth(row) {
-        const taskDuration = row.end.getTime() - row.start.getTime();
-        const chartDuration = this.endDate.getTime() - this.startDate.getTime();
-        return (taskDuration / chartDuration) * 100 + "%";
+      // ----subform----
+      /** 复选框选中数据 */
+      handleTableCreatorTableColumnSelectionChange (selection) {
+        this.checkedTableCreatorTableColumn = selection.map((item) => item.index)
       },
-      formatDate(date) {
-        return date.toLocaleDateString("en-US", {
-          month: "short",
-          day: "numeric"
-        });
-      }
-
+      /** 添加列按钮操作 */
+      addBetweenRow (index) {
+        let obj = {}
+        obj.name = ''
+        obj.comment = ''
+        obj.type = ''
+        obj.length = ''
+        obj.isNull = ''
+        obj.createBy = ''
+        obj.updateTime = ''
+        obj.createTime = ''
+        obj.groupId = ''
+        obj.updateBy = ''
+        obj.uuid = this.uuid(8,16)
+        this.columnForm.tableCreatorTableColumnList.splice(index, 0, obj)
+      },
+      deleteRow (index) {
+        this.columnForm.tableCreatorTableColumnList.splice(index - 1, 1)
+      },
+      ifLengthDisabledType (type) {
+        return type !== 'varchar'
+      },
+      uuid (len, radix) {
+        let chars = '0123456789abcdefghijklmnopqrstuvwxyz'
+        let uuid = []; let i
+        radix = radix || chars.length
+        if (len) {
+          for (i = 0; i < len; i++) {
+            uuid[i] = chars[0 | Math.random() * radix]
+          }
+        } else {
+          var r
+        }
+        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
+        uuid[14] = '4'
+        for (i = 0; i < 36; i++) {
+          if (!uuid[i]) {
+            r = 0 | Math.random() * 16
+            uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r]
+          }
+        }
+        return uuid.join('')
+      },
+      /** 建表工具-列信息序号 */
+      rowTableCreatorTableColumnIndex ({ row, rowIndex }) {
+        row.index = rowIndex + 1
+      },
     }
   }
 </script>
-  
+
 <style scoped>
-.gantt-chart {
-  overflow-x: auto;
+::v-deep.column-table td{
+  padding: 0!important;
 }
-.gantt-item {
-  height: 20px;
-  background-color: #409eff;
+::v-deep.column-table .el-form-item__content .el-form-item__error--inline{
+  position: absolute;
+  transform: translate(-20px,8px);
+  text-align: right;
+  pointer-events: none;
+  font-size: 13px;
+}
+::v-deep.column-table .cell{
+  overflow: unset !important;
+}
+::v-deep.column-table .el-form-item__content{
+  display: flex;
+  flex-direction: column;
+}
+.drag-line-box{
+  display: flex;
+  gap:3px;
+  flex-direction: column;
+  width: 15px;
+}
+.drag-line-box .drag-line{
+  width:100%;
+  height: 1px;
+  background: #cfcfcf;
+}
+.add-block{
+  padding: 5px;
+  border: 1px solid #EBEEF5;
+  border-top: none;
+  display: flex;
+  justify-content: center;
+}
+.allowDrag{
+  cursor: move
 }
 </style>
+
