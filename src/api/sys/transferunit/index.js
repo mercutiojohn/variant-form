c0cadd5971e0a282736e437a50a6cf6f31521128
Mercutio <mercutio_john@foxmail.com>
2023-04-21 14:41:04 +0800

    api test

--- src/api/sys/transferunit/index.js
+++ src/api/sys/transferunit/index.js
@@ -1,6 +1,9 @@
-// import request from '../../plugin/axios'
+// import axiosInstance from '../../plugin/axios';
 
-function promiseWrap (data) {
+// import request from '../../plugin/axios'
+// let axiosInstance = false;
+
+function promiseWrap (inData, data) {
   let callback = function nullFunc() {}
   let promise = new window.Promise(function (resolve, reject) {
     callback = function(response, error) {
@@ -10,7 +13,7 @@ function promiseWrap (data) {
   try {
     // const result = JSON.stringify(data)
     const result = data
-    console.log('[mock-api]', data)
+    console.log('[mock-api]','data:', inData, 'res:', data)
     callback(result)
   } catch (error) {
     callback(data, 'error')
@@ -20,89 +23,89 @@ function promiseWrap (data) {
 }
 
 // 获取承办单位及下属人员列表
-export function getNodeData(data, axiosInstance) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: "/addTransferunit/transferunit/getNodeData",
-      method: "post",
-      data
-    });
-  }
-  else {
+export function getNodeData(data) {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: "/addTransferunit/transferunit/getNodeData",
+  //     method: "post",
+  //     data
+  //   });
+  // }
+  // else {
     const res = require('./nodeData.json')
-    return promiseWrap(res)
-  }
+    return promiseWrap(data, res)
+  // }
 } 
 
 // 查询下一步操作拉出常用用户数据
-export function getCommonUseData (axiosInstance) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: '/addTransferunit/transferunit/getCommonUseData',
-      method: 'post'
-    })
-  }
-  else {
-    return promiseWrap()
-  }
+export function getCommonUseData () {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: '/addTransferunit/transferunit/getCommonUseData',
+  //     method: 'post'
+  //   })
+  // }
+  // else {
+    return promiseWrap({}, {})
+  // }
 
 }
 
 // 添加常用人数据
-export function addContacts (axiosInstance, data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: '/addTransferunit/transferunit/addContacts',
-      method: 'post',
-      data
-    })
-  }
-  else {
-    return promiseWrap()
-  }
+export function addContacts (data) {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: '/addTransferunit/transferunit/addContacts',
+  //     method: 'post',
+  //     data
+  //   })
+  // }
+  // else {
+    return promiseWrap(data, {})
+  // }
 }
 
 // 获取本单位内部人员
-export function getPersonData(axiosInstance, data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: "/addTransferunit/transferunit/getPersonData",
-      method: "post",
-      data
-    });
-  }
-  else {
+export function getPersonData(data) {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: "/addTransferunit/transferunit/getPersonData",
+  //     method: "post",
+  //     data
+  //   });
+  // }
+  // else {
     const res = require('./personData.json')
-    return promiseWrap(res)
-  }
+    return promiseWrap(data, res)
+  // }
 } 
 
 // 获取当前机构的副节点(内部)
-export function getParentId(axiosInstance, data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: "/addTransferunit/transferunit/getParentId",
-      method: "post",
-      data
-    });
-  }
-  else {
-    return promiseWrap({})
-  }
+export function getParentId(data) {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: "/addTransferunit/transferunit/getParentId",
+  //     method: "post",
+  //     data
+  //   });
+  // }
+  // else {
+    return promiseWrap(data, {})
+  // }
 } 
 
 // 查询下一步操作拉出的本机构的用户树数据
-export function getGroupUserTree (axiosInstance, data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: '/addTransferunit/transferunit/getGroupUserTree',
-      method: 'post',
-      data
-    })
-  }
-  else {
-    return promiseWrap({})
-  }
+export function getGroupUserTree (data) {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: '/addTransferunit/transferunit/getGroupUserTree',
+  //     method: 'post',
+  //     data
+  //   })
+  // }
+  // else {
+    return promiseWrap(data, {})
+  // }
 }
 
 // -------------------
--- src/extension/samples/userChoose/api/transferunit/index.js
+++ src/extension/samples/userChoose/api/transferunit/index.js
@@ -1,7 +1,7 @@
 // import axiosInstance from '../../plugin/axios';
 
 // import request from '../../plugin/axios'
-let axiosInstance = false;
+// let axiosInstance = false;
 
 function promiseWrap (inData, data) {
   let callback = function nullFunc() {}
@@ -24,88 +24,88 @@ function promiseWrap (inData, data) {
 
 // 获取承办单位及下属人员列表
 export function getNodeData(data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: "/addTransferunit/transferunit/getNodeData",
-      method: "post",
-      data
-    });
-  }
-  else {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: "/addTransferunit/transferunit/getNodeData",
+  //     method: "post",
+  //     data
+  //   });
+  // }
+  // else {
     const res = require('./nodeData.json')
     return promiseWrap(data, res)
-  }
+  // }
 } 
 
 // 查询下一步操作拉出常用用户数据
 export function getCommonUseData () {
-  if(axiosInstance){
-    return axiosInstance({
-      url: '/addTransferunit/transferunit/getCommonUseData',
-      method: 'post'
-    })
-  }
-  else {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: '/addTransferunit/transferunit/getCommonUseData',
+  //     method: 'post'
+  //   })
+  // }
+  // else {
     return promiseWrap({}, {})
-  }
+  // }
 
 }
 
 // 添加常用人数据
 export function addContacts (data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: '/addTransferunit/transferunit/addContacts',
-      method: 'post',
-      data
-    })
-  }
-  else {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: '/addTransferunit/transferunit/addContacts',
+  //     method: 'post',
+  //     data
+  //   })
+  // }
+  // else {
     return promiseWrap(data, {})
-  }
+  // }
 }
 
 // 获取本单位内部人员
 export function getPersonData(data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: "/addTransferunit/transferunit/getPersonData",
-      method: "post",
-      data
-    });
-  }
-  else {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: "/addTransferunit/transferunit/getPersonData",
+  //     method: "post",
+  //     data
+  //   });
+  // }
+  // else {
     const res = require('./personData.json')
     return promiseWrap(data, res)
-  }
+  // }
 } 
 
 // 获取当前机构的副节点(内部)
 export function getParentId(data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: "/addTransferunit/transferunit/getParentId",
-      method: "post",
-      data
-    });
-  }
-  else {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: "/addTransferunit/transferunit/getParentId",
+  //     method: "post",
+  //     data
+  //   });
+  // }
+  // else {
     return promiseWrap(data, {})
-  }
+  // }
 } 
 
 // 查询下一步操作拉出的本机构的用户树数据
 export function getGroupUserTree (data) {
-  if(axiosInstance){
-    return axiosInstance({
-      url: '/addTransferunit/transferunit/getGroupUserTree',
-      method: 'post',
-      data
-    })
-  }
-  else {
+  // if(axiosInstance){
+  //   return axiosInstance({
+  //     url: '/addTransferunit/transferunit/getGroupUserTree',
+  //     method: 'post',
+  //     data
+  //   })
+  // }
+  // else {
     return promiseWrap(data, {})
-  }
+  // }
 }
 
 // -------------------
